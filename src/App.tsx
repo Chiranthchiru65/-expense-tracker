3189c0ff5a7619afabc140b4be1af44853e883b5
chiranth2428 <chiranth@interactivedigitalmedia.in>
2025-07-06 17:03:30 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+import { combine } from "zustand/middleware";
+import { useCounterStore } from "./store/CounterStore";
+function App() {
+  const { count, increment, decrement } = useCounterStore();
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline">Counter:{count}</h1>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
